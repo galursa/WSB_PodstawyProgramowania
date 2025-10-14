37e42002387b1d06255a4ca1add0d6b5e6078b34
jakula <jakula@poczta.fm>
2025-10-14 11:00:33 +0200

    poprawki

--- Wykłady/02_instrukcjeWarunkowe/sorting3NumbersWithIf.cpp
+++ Wykłady/02_instrukcjeWarunkowe/sorting3NumbersWithIf.cpp
@@ -12,25 +12,19 @@ int main(int argc, char** argv) {
 	{
 		if (a2<=a3)
 			cout << a1 << " " << a2 << " "<< a3 << "\n";
-		else
-		{
-			if (a1<=a3)
-				cout << a1 << " " << a3 << " "<< a2 << "\n";
-			else
-				cout << a3 << " " << a1 << " "<< a2 << "\n";
-		}
+		else if (a1<=a3)
+            cout << a1 << " " << a3 << " "<< a2 << "\n";
+        else
+			cout << a3 << " " << a1 << " "<< a2 << "\n";
 	}
 	else
 	{
 		if (a1<=a3)
 			cout << a2 << " " << a1 << " "<< a3 << "\n";
-		else
-		{
-			if (a1<=a3)
-				cout << a2 << " " << a3 << " "<< a1 << "\n";
-			else
-				cout << a3 << " " << a2 << " "<< a1 << "\n";	
-		}
-	}	
+		else if (a1<=a3)
+			cout << a2 << " " << a3 << " "<< a1 << "\n";
+        else
+			cout << a3 << " " << a2 << " "<< a1 << "\n";
+	}
 	return 0;
 }
